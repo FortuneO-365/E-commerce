b9e2437120a6c978b9b0e6a04681d3d8b831d0eb
Fortune <obiohafortune22@gmail.com>
2024-09-23 10:32:51 +0100

    initial commits

---
+++ E-commerce/src/pages/nav/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <>
+            <div className="top-nav">
+                
+            </div>
+        
+        </>
+    )
+}
+
+export default Nav
